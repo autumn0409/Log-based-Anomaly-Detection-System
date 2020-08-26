c2ab26e434decfc00ee56a4aff051ca4f81ca60e
Adam Su <autumn0409.cs08g@nctu.edu.tw>
2020-08-26 11:55:11 +0000

    Bi LSTM model

---
+++ train.py
@@ -0,0 +1,28 @@
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Bidirectional
+from keras_self_attention import SeqWeightedAttention
+
+
+# hyper-parameters
+batch_size = 16
+epochs = 20
+
+timesteps = 20
+vec_len = 300
+rnn_units = 32
+
+# model
+model = Sequential()
+model.add(Bidirectional(LSTM(rnn_units, return_sequences=True),
+                        input_shape=(timesteps, vec_len)))
+model.add(SeqWeightedAttention())
+model.add(Dense(2, activation='softmax'))
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop', metrics=['accuracy'])
+print(model.summary())
+
+
+# train
+# model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
+
+# model.save('my_model.h5')
